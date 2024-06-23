b2c577c313018779c3571e43c2931f540b940d6f
Kajetan <knas.kajetan@gmail.com>
2024-06-24 00:46:11 +0200

    implement the full layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBox from '@/components/TotalBox';
+import RightSideBar from '@/components/RightSideBar';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Kajetan', lastName: 'Knaś', email: 'knas.kajetan@gmail.com' };
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="start"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Manage your account and transactions with ease."
+          />
+
+          <TotalBox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250}
+          />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSideBar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{currentBalance: 212.32}, {currentBalance: 531.86}]}
+      />
+    </section>
   )
 }
 
